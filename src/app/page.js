e25b7fd786d9f78c7527c749e9a85628c7c02028
Abhishek khare <ghostabhi323@gmail.com>
2024-03-02 21:35:53 +0530

    Remove unused code and gradients in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -104,13 +104,11 @@ export default function Home() {
           Why Choose Us?
         </h1>
         <div className="w-[40rem] h-40 relative">
-          {/* Gradients */}
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
           <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
           <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
   
-          {/* Core component */}
           <SparklesCore
             background="transparent"
             minSize={0.4}
@@ -119,8 +117,6 @@ export default function Home() {
             className="w-full h-full"
             particleColor="#FFFFFF"
           />
-  
-          {/* Radial Gradient to prevent sharp edges */}
           <div className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
         </div>
         <div className="w-1/2">
